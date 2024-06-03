9749d29e391c718f7cd9571f81fbdfc0fd881a12
yidest <wh2385@nyu.edu>
2024-06-03 14:32:41 -0400

    finished the assignment

--- solutions.py
+++ solutions.py
@@ -27,6 +27,12 @@ def is_square():
       :returns: True if square (i.e. if equal length and height), False otherwise.
     """
     #### write your solution for this function below here. ####
+    width=float(input("width: "))
+    height=float(input("height: "))
+    if width==height:
+        return True
+    else:
+        return False
 
 
 def get_greatest():
@@ -38,6 +44,14 @@ def get_greatest():
     :returns: the greatest of the two input numbers, as an int.
     """
     #### write your solution for this function below here. ####
+    num1=int(input("first number: "))
+    num2=int(input("second number: "))
+    if num1>num2:
+        return num1
+    elif num1<num2:
+        return num2
+    else:
+        return num1
 
 
 def get_bmi_category():
@@ -60,6 +74,25 @@ def get_bmi_category():
       :returns: The name of the BMI statistical category, based on the inputted height and weight.
     """
     #### write your solution for this function below here. ####
+    height=float(input("your height:"))
+    weight=float(input("your weight: "))
+    bmi=(703*weight)/height**2
+    if bmi<15.0:
+        return "Very severely underweight"
+    elif bmi>=15.0 and bmi<16.0:
+        return "Severely underweight"
+    elif bmi>=16.0 and bmi<18.5:
+        return "Underweight"
+    elif bmi>=18.5 and bmi<25.0:
+        return "Normal"
+    elif bmi>=25.0 and bmi<30.0:
+        return "Overweight"
+    elif bmi>=30.0 and bmi<35.0:
+        return "Moderately obese"
+    elif bmi>=35.0 and bmi<40.0:
+        return "Severely obese"
+    else:
+        return "Very severely obese"
 
 
 def get_discount():
@@ -72,6 +105,15 @@ def get_discount():
       :returns: The cost of the masks, after any discounts, e.g. "$4,000" for 1000 masks.
     """
     #### write your solution for this function below here. ####
+    mask_num=int(input("how many masks do you want: "))
+    total_price=mask_num*5
+    if total_price>=5000:
+        total_price=total_price*0.8
+    else:
+        total_price=total_price
+    format_price=format(int(total_price),",")
+    final_form_price="$"+str(format_price)
+    return final_form_price
 
 
 def is_leap_year():
@@ -85,3 +127,8 @@ def is_leap_year():
         get_year()
     )  # this line is given to you - the variable, year, holds the current year
     #### write your solution for this function below here. ####
+    weather_leap=int(year)%4
+    if weather_leap==0:
+        return True
+    else:
+        return False
